0e539128237977b9e47fad8fefd30c8276906524
Royal Davanam <royaldavanam02@gmail.com>
2026-01-07 23:43:00 +0530

    Solved Basics Problems

--- 1_Basics/Basics.java
+++ 1_Basics/Basics.java
@@ -1,6 +1,53 @@
+import java.util.Scanner;
 public class Basics {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("ROYAL");
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Hello world!");
+        // System.out.println("ROYAL");
+
+        // //  Area of a Rectangle and Triangle
+        // System.out.println("Calculateing the Area of a Rectangle");
+        // System.out.print("Enter the length of the Rectangle:");
+        // float length = sc.nextFloat();
+        // System.out.print("Enter the width Rectangle:");
+        // float width = sc.nextFloat();
+        // System.out.println("Area of the Rectangle is:" + (length * width));
+        // System.out.println("Calculateing the Area of a Rectangle");
+        // System.out.println("Calculateing the Area of a Triangle");
+        // System.out.print("Enter the base of the Triangle:");
+        // float base = sc.nextFloat();
+        // System.out.print("Enter the height of the Triangle:");
+        // float height = sc.nextFloat();
+        // System.out.println("Area of the Triangle is:" + (0.5 * base * height));
+   
+        
+         //Simple Interest Calculator 
+        // System.out.println("Calucation of Simple Interest:");
+        // System.out.print("Enter the Principle amount:");
+        // int principle = sc.nextInt();
+        // System.out.print("Enter the Rate of interest:");
+        // float rate = sc.nextFloat();
+        // System.out.print("Enter the Time:");
+        // short time = sc.nextShort();
+        // System.out.println("Simple Interest is:" + (principle * rate * time) / 100);
+
+        // //Temperature Conversion. Converting  Celsius to Fahrenheit.
+        // System.out.println("Temperature Conversion from Celsius to Fahrenheit:");
+        // System.out.print("Enter the value of Celsius:");
+        // float celsius = sc.nextFloat();
+        // System.out.println("Celsius to Fahrenheit Conversion:" + (celsius * 9 / 5) + 32);
+
+        
+// // Take name as input and print a greeting message for that particular name.
+// System.out.println("Greeting message");
+// System.out.print("Enter your name:");
+// String name = sc.nextLine();
+// System.out.println("Hi " + name + " How are you ?");
+
+
+
+
+      
+
     }
 }
