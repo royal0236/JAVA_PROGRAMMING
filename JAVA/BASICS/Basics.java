ba23f14520c8a3a5e64fa93793081c495b886abc
Royal Davanam <royaldavanam02@gmail.com>
2026-01-27 17:25:15 +0530

    Add Basics.java

--- JAVA/BASICS/Basics.java
+++ JAVA/BASICS/Basics.java
@@ -21,7 +21,7 @@ public class Basics {
              System.out.println("Total marks obtained:" + result);
         // Create a variable for year of birth and calculate your current age. 
              int year = 2005;
-             System.out.println("My current age is:" + (2026 - 2005));
+             System.out.println("My current age is:" + (2026 - year));
         // Store length and breadth of a rectangle in variables and print: Area , Perimeter
             int length = 3;
             int breadth = 10;
@@ -120,20 +120,76 @@ public class Basics {
                     System.out.print("Enter the second number:");
                     int SECond_Number = sc.nextInt();
                     System.out.println("Quotient of the two numbers is:" + FIrst_Number / SECond_Number + "\n"
-                     + "Remainder for the to numbers is:" + FIrst_Number % SECond_Number);
+                       + "Remainder for the to numbers is:" + FIrst_Number % SECond_Number);
+                //Simple Interest Calculator 
+                    System.out.println("Calculateing the Simple Interest Calulator");
+                    System.out.print("Enter the value of Principle:");
+                    double Principle=sc.nextDouble();
+                    System.out.print("Enter the value of Rate:");
+                    double Rate=sc.nextDouble();
+                    System.out.print("Enter the value of Time:");
+                    float Time=sc.nextFloat();
+                    System.out.println("Simple Interest is:" + (Principle * Rate * Time) / 100);
+                //Temperature Conversion. Converting  Celsius to Fahrenheit.
+                  System.out.println("Temperature Conversion");
+                  System.out.print("Enter the value of celsius:");
+                  float celsius=sc.nextFloat();
+                  double Fahrenheit=(celsius * 9/5) + 32;
+                  System.out.println("Temperature Conversion Converting  Celsius to Fahrenheit is:"
+                          + Fahrenheit);
+                     //   ASCII Value of a Character
+                  System.out.println("ASCII value of a character");
+                  System.out.print("Enter the character:");
+                  char character = sc.next().charAt(0);
+                  int ASCII_value=(int)character;
+                  System.out.println("The ASCII value of '" + character + "' is: " +  ASCII_value);
                 //  Convert minutes into hours and remaining minutes.
                     System.out.print("Enter the minuntes:");
                     int minutes = sc.nextInt();
                     System.out.println("Minutes entered is:" + minutes + " Converted into hours and remaining minuntes:" + minutes / 60 + " hrs and"
-                         + minutes % 60 + " min");
+                            + minutes % 60 + " min");
+               // Find Largest of Three Numbers
+                  System.out.println("Find the largest number");
+                  System.out.print("Enter the first number:");
+                  int first_number1=sc.nextInt();
+                  System.out.print("Enter the second number:");
+                  int second_number2=sc.nextInt();
+                   System.out.print("Enter the third number:");
+                   int third_number3=sc.nextInt();
+                   int largest_number=Math.max(first_number1 , Math.max(second_number2, third_number3));
+                    System.out.println("The Largest Number is:" + largest_number);
+                    int smallest_number=Math.min(first_number1 , Math.min(second_number2, third_number3));
+                    System.out.println("The Smallest Number is:" + smallest_number);
+       
                 //  Take a 3-digit number and print the sum of its digits (Hint: use / and %)
-                    System.out.print("Enter a three digit number:");
-                    int i = sc.nextInt();
-                    int j = i % 10;
-                    i = i / 10;
-                    int k = i % 10;
-                    int l = i / 10;
-                    System.out.println(j + k + l);
+                    // System.out.print("Enter a three digit number:");
+                    // int i = sc.nextInt();
+                    // int j = i % 10;
+                    // i = i / 10;
+                    // int k = i % 10;
+                    // int l = i / 10;
+                    // System.out.println(j + k + l);
+                    //  System.out.print("Enter a two digit number and sum :"); 1 + 2 + 3 = 6
+                     int i = sc.nextInt();
+                     int a = i % 10; // 123 = 3
+                     i = i / 10; // 123/10=12
+                     int b = i % 10; //12%10=2
+                     int c = i / 10; // 12/10=1
+                  System.out.println(a + b + c);
+            //    Swap Two Numbers.
+                     System.out.println("Swaping of Two numbers");
+                    System.out.print("Enter your First number:");
+                    int first=sc.nextInt(); // 1
+                     System.out.print("Enter your Second number:");
+                     int second=sc.nextInt(); // 2
+                     int temp=first; // temp=1
+                     first=second; // first=2
+                     second=temp; // second=1
+                     System.out.println("First number is swaped to second number:" + first
+                             + "\nSecond number is swapped to first number:" + second);
+                    
+  
+
 
 
 
