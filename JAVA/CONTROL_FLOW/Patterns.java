6abaae54d7dfe1a65f9079add5a7ee59e6288190
Royal Davanam <royaldavanam02@gmail.com>
2026-02-06 20:10:30 +0530

    Patterns Problems

---
+++ JAVA/CONTROL_FLOW/Patterns.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // for (int row = 1; row <= 5; row++) { // Outer: Rows
+        //     for (int col = 1; col <= 5; col++) { // Inner: Columns
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(); // New line after each row
+        // }
+
+        // int n = sc.nextInt(); // 5
+        // for (int j = 1; j <= n; j++) {
+        //    // System.out.println("*");
+        //     // System.out.print("*");
+        // }
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     System.out.print(i + " ");
+        // }
+        // int i, j, n;
+        //  n = sc.nextInt(); // 5
+        // for ( i=1; i <= n; i++ ) // rows 1 2 3 4 5  
+        // {
+        //     for ( j = 1; j >= i; j++) { // columns  
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println("");
+        //  }
+        // int i, j, n;
+        // n = sc.nextInt();
+        // for (i = 1; i <= n; i++) {
+        //     if (i % 2 == 0) {
+        //         for (j = 1; j <= n; j++) {
+        //             System.out.print(j + "");
+        //         }
+        //     } else {
+        //         for (j = n; j >= 1; j--) {
+        //             System.out.print(j + "");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        //  Pattern 1: Simple Square (4x4)
+        // int n = sc.nextInt(); // 4
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print("* ");
+        //     }
+
+        //     System.out.println();
+        // }
+        //   Pattern 2: Right Triangle
+        // int n = sc.nextInt(); 
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         // System.out.print("* ");
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // Pattern 3: Inverted Right Triangle
+        // int n = sc.nextInt();
+        // for (int i = n; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         // System.out.print(j);
+        //         System.out.print("* ");
+        //     }
+        //      System.out.println();
+        // }
+        //Pattern 4: Left Triangle
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int k = 1; k <= i; k++) {
+        //          System.out.print("* ");  
+        //     }
+        //     System.out.println();
+        // }
+        // Pattern 5: Number Right Triangle  
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        
+
+    }
+    
+}
